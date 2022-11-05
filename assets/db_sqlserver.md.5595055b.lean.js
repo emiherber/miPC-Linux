e1ae8557c66bffec172e4963e5a727c307819189
emiherber <emiherber@users.noreply.github.com>
2022-11-05 18:36:07 +0000

    deploy: 015bc91dd59d08cc3c8b72af0ee6ed05821cecd8

---
+++ assets/db_sqlserver.md.5595055b.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,a as l}from"./app.d825676a.js";const i=JSON.parse('{"title":"Cambiar OWNER","description":"","frontmatter":{},"headers":[],"relativePath":"db/sqlserver.md"}'),p={name:"db/sqlserver.md"},o=l("",2),e=[o];function C(c,t,r,A,y,D){return n(),a("div",null,e)}const E=s(p,[["render",C]]);export{i as __pageData,E as default};
