c37d4d3d440645ebdaea83bce2ba5125a1739d7b
emiherber <emiherber@users.noreply.github.com>
2022-12-03 21:48:26 +0000

    deploy: 39b647e7fbd56a59e853e831e90fc881f7942692

--- assets/ubuntu_terminal.md.068ad1d8.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as n,a as p}from"./app.460540a2.js";const F=JSON.parse('{"title":"Terminal","description":"","frontmatter":{},"headers":[],"relativePath":"ubuntu/terminal.md","lastUpdated":1667672845000}'),l={name:"ubuntu/terminal.md"},o=p("",8),e=[o];function r(t,c,D,y,A,C){return n(),a("div",null,e)}const h=s(l,[["render",r]]);export{F as __pageData,h as default};
